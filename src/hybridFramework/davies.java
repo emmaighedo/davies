42a766a66e63ad4b654c42bb2110714af19c9851
emmaighedo <emmaighedo@yahoo.com>
2020-12-17 09:19:17 +0000

    amended

--- src/hybridFramework/davies.java
+++ src/hybridFramework/davies.java
@@ -1,13 +1,8 @@
 package hybridFramework;
 
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class davies {
